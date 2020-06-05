97b402971846b6bb7e4bf414f59b2991e492c2a1
Yihua REN <yi.ren@utbm.fr>
2020-06-05 11:01:32 +0200

    Initialisation JSP

--- src/main/java/fr/utbm/lo54projet/Lo54projetApplication.java
+++ src/main/java/fr/utbm/lo54projet/Lo54projetApplication.java
@@ -2,8 +2,10 @@ package fr.utbm.lo54projet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement(proxyTargetClass = true)
 public class Lo54projetApplication {
 
 	public static void main(String[] args) {
--- src/main/java/fr/utbm/lo54projet/Service/CourseService.java
+++ src/main/java/fr/utbm/lo54projet/Service/CourseService.java
@@ -8,21 +8,13 @@ import javax.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import fr.utbm.lo54projet.Entity.Course;
-<<<<<<< HEAD
 import fr.utbm.lo54projet.Repository.CourseDao;
-=======
-import fr.utbm.lo54projet.Repository.CourseRepository;
->>>>>>> c3059727bf17ea75c10c82c4f3c99c057bad82b4
 
 @Service
 public class CourseService {
 
 	@Resource
-<<<<<<< HEAD
 	private CourseDao courseRepository;
-=======
-	private CourseRepository courseRepository;
->>>>>>> c3059727bf17ea75c10c82c4f3c99c057bad82b4
 
 	@Transactional
 	public List<Course> getCourse() {
